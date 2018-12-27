2d6244c55086692b72950bb8a2385f549a2145d6
tushar <tushar.h.dahibhate@gmail.com>
2018-12-26 23:50:51 -0500

    Initial Commit

---
+++ mymodule.py
@@ -0,0 +1,2 @@
+def func():
+    print("Hello there! I am currently in module.py")
---
+++ myprogram.py
@@ -0,0 +1,5 @@
+from MyMainPackage import some_main_script
+from MyMainPackage.SubPackage import my_subscript
+
+my_subscript.sub_report()
+some_main_script.report_main()
